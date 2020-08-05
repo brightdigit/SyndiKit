20e5c656dbfa9abe4ba8745feb3a97c4ccb62866
Leo Dion <leogdion@brightdigit.com>
2020-08-05 17:12:10 -0400

    refactoring HTML

--- Sources/OrchardNestServer/Controllers/Routing/HTMLController.swift
+++ Sources/OrchardNestServer/Controllers/Routing/HTMLController.swift
@@ -6,6 +6,87 @@ import Vapor
 
 struct InvalidDatabaseError: Error {}
 
+extension Node where Context == HTML.BodyContext {
+  static func filters() -> Self {
+    .nav(
+      .class("posts-filter clearfix row"),
+      .ul(
+        .class("column"),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-calendar")), .text(" Latest"))),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-cogs")), .text(" Development"))),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-bullhorn")), .text(" Marketing"))),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-brush")), .text(" Design"))),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-podcast")), .text(" Podcasts"))),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-video")), .text(" Videos"))),
+        .li(.a(.class("button"), .href("#"), .i(.class("el el-envelope")), .text(" Newsletters")))
+      )
+    )
+  }
+}
+
+extension Node where Context == HTML.BodyContext {
+  static func header() -> Self {
+    .header(
+      .class("container"),
+      .nav(
+        .class("row"),
+        .ul(
+          .class("column"),
+          .li(.a(.href("#"), .i(.class("el el-home")), .text(" Home"))),
+          .li(.a(.href("#"), .i(.class("el el-info-circle")), .text(" About"))),
+          .li(.a(.href("#"), .i(.class("el el-question-sign")), .text(" Support")))
+        ),
+        .ul(.class("float-right column"),
+            .li(.a(.href("#"), .i(.class("el el-github")), .text(" github"))),
+            .li(.a(.href("#"), .i(.class("el el-twitter")), .text(" twitter"))))
+      ),
+      .div(
+        .class("row"),
+        .h1(
+          .class("column"),
+          .img(
+            .class("logo"),
+            .src("/images/logo.svg")
+          ),
+          .text("&nbsp;OrchardNest")
+        )
+      ),
+      div(
+        .class("row"),
+        .p(
+          .class("tagline column"),
+          .text("Swift Articles and News")
+        )
+      )
+    )
+  }
+}
+
+extension Node where Context == HTML.DocumentContext {
+  static func head(withSubtitle subtitle: String) -> Self {
+    return
+      .head(
+        .title("OrchardNest - \(subtitle)"),
+        .meta(.charset(.utf8)),
+
+        .link(.rel(.appleTouchIcon), .sizes("180x180"), .href("/apple-touch-icon.png")),
+        .link(.rel(.appleTouchIcon), .type("image/png"), .sizes("32x32"), .href("/favicon-32x32.png")),
+        .link(.rel(.appleTouchIcon), .type("image/png"), .sizes("16x16"), .href("/favicon-16x16.png")),
+        .link(.rel(.manifest), .href("/site.webmanifest")),
+        .link(.rel(.maskIcon), .href("/safari-pinned-tab.svg"), .color("#5bbad5")),
+        .meta(.name("msapplication-TileColor"), .content("#2b5797")),
+        .meta(.name("theme-color"), .content("#ffffff")),
+        .link(.rel(.stylesheet), .href("/styles/elusive-icons/css/elusive-icons.min.css")),
+
+        .link(.rel(.stylesheet), .href("/styles/normalize.css")),
+
+        .link(.rel(.stylesheet), .href("/styles/milligram.css")),
+        .link(.rel(.stylesheet), .href("/styles/style.css")),
+        .link(.rel(.stylesheet), .href("https://fonts.googleapis.com/css2?family=Catamaran:wght@100;400;800&display=swap"))
+      )
+  }
+}
+
 extension Node where Context == HTML.ListContext {
   static func li(forEntryItem item: EntryItem) -> Self {
     return
@@ -158,80 +239,31 @@ struct HTMLController {
       }
       .map { (items) -> HTML in
         HTML(
-          .head(
-            .title("OrchardNest - Swift Articles and News"),
-            .meta(.charset(.utf8)),
-
-            .link(.rel(.appleTouchIcon), .sizes("180x180"), .href("/apple-touch-icon.png")),
-            .link(.rel(.appleTouchIcon), .type("image/png"), .sizes("32x32"), .href("/favicon-32x32.png")),
-            .link(.rel(.appleTouchIcon), .type("image/png"), .sizes("16x16"), .href("/favicon-16x16.png")),
-            .link(.rel(.manifest), .href("/site.webmanifest")),
-            .link(.rel(.maskIcon), .href("/safari-pinned-tab.svg"), .color("#5bbad5")),
-            .meta(.name("msapplication-TileColor"), .content("#2b5797")),
-            .meta(.name("theme-color"), .content("#ffffff")),
-            .link(.rel(.stylesheet), .href("/styles/elusive-icons/css/elusive-icons.min.css")),
-
-            .link(.rel(.stylesheet), .href("/styles/normalize.css")),
-
-            .link(.rel(.stylesheet), .href("/styles/milligram.css")),
-            .link(.rel(.stylesheet), .href("/styles/style.css")),
-            .link(.rel(.stylesheet), .href("https://fonts.googleapis.com/css2?family=Catamaran:wght@100;400;800&display=swap"))
-          ),
+          .head(withSubtitle: "Swift Articles and News"),
           .body(
-            .div(
+            //            .div(
+//              .class("container"),
+//              .div(
+//                .class("row"),
+//                .div(
+//                  .class("column"),
+            .header(),
+            .main(
               .class("container"),
-              .div(
+              .filters(),
+              .section(
                 .class("row"),
-                .div(
-                  .class("column"),
-                  .header(
-                    .nav(
-                      .class("clearfix"),
-                      .ul(
-                        .li(.a(.href("#"), .i(.class("el el-home")), .text(" Home"))),
-                        .li(.a(.href("#"), .i(.class("el el-info-circle")), .text(" About"))),
-                        .li(.a(.href("#"), .i(.class("el el-question-sign")), .text(" Support")))
-                      ),
-                      .ul(.class("float-right"),
-                          .li(.a(.href("#"), .i(.class("el el-github")), .text(" github"))),
-                          .li(.a(.href("#"), .i(.class("el el-twitter")), .text(" twitter"))))
-                    ),
-                    .h1(
-                      .img(
-                        .class("logo"),
-                        .src("/images/logo.svg")
-                      ),
-                      .text("&nbsp;OrchardNest")
-                    ),
-                    .p(
-                      .class("tagline"),
-                      .text("Swift Articles and News")
-                    )
-                  ),
-                  .main(
-                    .nav(
-                      .class("posts-filter clearfix"),
-                      .ul(
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-calendar")), .text(" Latest"))),
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-cogs")), .text(" Development"))),
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-bullhorn")), .text(" Marketing"))),
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-brush")), .text(" Design"))),
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-podcast")), .text(" Podcasts"))),
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-video")), .text(" Videos"))),
-                        .li(.a(.class("button"), .href("#"), .i(.class("el el-envelope")), .text(" Newsletters")))
-                      )
-                    ),
-
-                    .ul(
-                      .class("articles"),
-                      .forEach(items) {
-                        .li(forEntryItem: $0)
-                      }
-                    )
-                  )
+                .ul(
+                  .class("articles column"),
+                  .forEach(items) {
+                    .li(forEntryItem: $0)
+                  }
                 )
               )
             )
+//                )
+//              )
+//            )
           )
         )
       }
