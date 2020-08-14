9ec964a1acc9e4e17b6930b475198e5b108ac520
Leo Dion <leogdion@brightdigit.com>
2020-08-14 11:45:10 -0400

    fixing url issue on Linux

--- Sources/OrchardNestServer/URL.swift
+++ Sources/OrchardNestServer/URL.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 extension URL {
-  func safeAppendingPathComponent (_ pathComponent: String) -> URL {
+  func safeAppendingPathComponent(_ pathComponent: String) -> URL {
     #if os(Linux)
-    if pathComponent.isEmpty {
-      return self
-    } else {
-      return self.appendingPathComponent(pathComponent)
-    }
+      if pathComponent.isEmpty {
+        return self
+      } else {
+        return appendingPathComponent(pathComponent)
+      }
     #else
-    return self.appendingPathComponent(pathComponent)
+      return appendingPathComponent(pathComponent)
     #endif
   }
 }
