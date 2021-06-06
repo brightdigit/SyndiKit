8fee6151f3ca8d7cb2a9eec71c0c9e645f4db2bd
Leo Dion <leogdion@brightdigit.com>
2021-06-06 16:28:53 -0400

    Unhide dependencies

--- Package.swift
+++ Package.swift
@@ -26,12 +26,12 @@ let package = Package(
     .package(name: "QueuesFluentDriver", url: "https://github.com/m-barthelemy/vapor-queues-fluent-driver.git", from: "0.3.8"),
     .package(name: "Plot", url: "https://github.com/johnsundell/plot.git", from: "0.8.0"),
     .package(url: "https://github.com/JohnSundell/Ink.git", from: "0.1.0"),
-//    .package(url: "https://github.com/shibapm/Komondor", from: "1.0.6"), // dev
-//    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
-//    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
-//    .package(url: "https://github.com/realm/SwiftLint", from: "0.41.0"), // dev
-//    .package(url: "https://github.com/brightdigit/Rocket", .branch("feature/yams-4.0.0")), // dev
-//    .package(url: "https://github.com/mattpolzin/swift-test-codecov", .branch("master")) // dev
+    .package(url: "https://github.com/shibapm/Komondor", from: "1.0.6"), // dev
+    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
+    .package(url: "https://github.com/realm/SwiftLint", from: "0.41.0"), // dev
+    .package(url: "https://github.com/brightdigit/Rocket", .branch("feature/yams-4.0.0")), // dev
+    .package(url: "https://github.com/mattpolzin/swift-test-codecov", .branch("master")) // dev
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
