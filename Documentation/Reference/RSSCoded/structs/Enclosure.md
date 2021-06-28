9d640dd9e9958209549b0cd06356d6dd7459ba8d
Leo Dion <leogdion@brightdigit.com>
2021-06-28 12:29:35 -0400

    fixed enclosure issue

--- Documentation/Reference/RSSCoded/structs/Enclosure.md
+++ Documentation/Reference/RSSCoded/structs/Enclosure.md
@@ -5,3 +5,16 @@
 ```swift
 public struct Enclosure: Codable
 ```
+
+## Methods
+### `init(from:)`
+
+```swift
+public init(from decoder: Decoder) throws
+```
+
+#### Parameters
+
+| Name | Description |
+| ---- | ----------- |
+| decoder | The decoder to read data from. |
--- Sources/RSSCoded/Enclosure.swift
+++ Sources/RSSCoded/Enclosure.swift
@@ -3,7 +3,13 @@ import Foundation
 public struct Enclosure: Codable {
   let url: URL
   let type: String
-  let length: Int
+  let length: Int?
+
+  enum CodingKeys: String, CodingKey {
+    case url
+    case type
+    case length
+  }
 
   var imageURL: URL? {
     guard type.starts(with: "image/") else {
@@ -19,6 +25,13 @@ public struct Enclosure: Codable {
     }
 
     return url
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: Self.CodingKeys)
+    url = try container.decode(URL.self, forKey: .url)
+    type = try container.decode(String.self, forKey: .type)
+    length = try? container.decode(Int.self, forKey: .length)
   }
 }
 
