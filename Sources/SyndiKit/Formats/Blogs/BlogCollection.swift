547f16532322c4095d44ef4e072eb992943ca88d
Leo Dion <leogdion@brightdigit.com>
2021-06-30 21:44:06 -0400

    adding dictionary values

--- Sources/SyndiKit/Formats/Blogs/BlogCollection.swift
+++ Sources/SyndiKit/Formats/Blogs/BlogCollection.swift
@@ -2,11 +2,17 @@ import Foundation
 
 public struct BlogCollection {
   let allSites: [BlogSite]
-  let languages: [LanguageType: Language]
-  let categories: [CategoryType: Category]
+  let languageDictionary: [LanguageType: Language]
+  let categoryDictionary: [CategoryType: Category]
   let languageIndicies: [LanguageType: Set<Int>]
   let categoryIndicies: [CategoryType: Set<Int>]
 
+  public func languages() -> Dictionary<LanguageType, Language>.Values {
+    return languageDictionary.values
+  }
+  public func categories() -> Dictionary<CategoryType, Category>.Values {
+    return categoryDictionary.values
+  }
   public func sites(
     withLanguage language: LanguageType? = nil,
     withCategory category: CategoryType? = nil
@@ -81,11 +87,11 @@ public struct BlogCollection {
       languages.append(language)
     }
 
-    self.categories = Dictionary(
+    self.categoryDictionary = Dictionary(
       grouping: categories,
       by: { $0.type }
     ).compactMapValues(Category.init)
-    self.languages = Dictionary(uniqueKeysWithValues: languages.map { ($0.type, $0) })
+    self.languageDictionary = Dictionary(uniqueKeysWithValues: languages.map { ($0.type, $0) })
     self.languageIndicies = languageIndicies
     self.categoryIndicies = categoryIndicies
     allSites = sites
