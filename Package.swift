e2e813795fb329a780d9c85993ea61e8f475a033
Leo Dion <leogdion@brightdigit.com>
2022-11-17 17:59:41 -0500

    fixing linting

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2.0
+// swift-tools-version:5.5
 // swiftlint:disable explicit_top_level_acl
 import PackageDescription
 
@@ -10,17 +10,9 @@ let package = Package(
       targets: ["SyndiKit"]
     )
   ],
-  // swiftlint:disable line_length
   dependencies: [
-    .package(url: "https://github.com/MaxDesiatov/XMLCoder", from: "0.13.1"),
-    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.1"), // dev
-    .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
-    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
-    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
-    .package(url: "https://github.com/shibapm/Rocket", from: "1.2.1"), // dev
-    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
+    .package(url: "https://github.com/MaxDesiatov/XMLCoder", from: "0.13.1")
   ],
-  // swiftlint:enable line_length
   targets: [
     .target(
       name: "SyndiKit",
@@ -32,41 +24,3 @@ let package = Package(
     )
   ]
 )
-
-#if canImport(PackageConfig)
-  import PackageConfig
-
-  let requiredCoverage: Int = 80
-
-  let config = PackageConfiguration([
-    "rocket": [
-      "steps": [
-        ["hide_dev_dependencies": ["package_path": "Package@swift-5.5.swift"]],
-        "hide_dev_dependencies",
-        "git_add",
-        ["commit": ["no_verify": true]],
-        "tag",
-        "unhide_dev_dependencies",
-        ["unhide_dev_dependencies": ["package_path": "Package@swift-5.5.swift"]],
-        "git_add",
-        ["commit": ["message": "Unhide dev dependencies"]]
-      ]
-    ],
-    "komondor": [
-      "pre-push": [
-        "swift test --enable-code-coverage --enable-test-discovery",
-        // swiftlint:disable:next line_length
-        "swift run swift-test-codecov .build/debug/codecov/SyndiKit.json --minimum \(requiredCoverage)"
-      ],
-      "pre-commit": [
-        "swift test --enable-code-coverage --enable-test-discovery --generate-linuxmain",
-        "swift run swiftformat .",
-        "swift run swiftlint autocorrect",
-//      "swift run sourcedocs generate build --clean --reproducible-docs --all-modules",
-        "git add .",
-        "swift run swiftformat --lint .",
-        "swift run swiftlint"
-      ]
-    ]
-  ]).write()
-#endif
