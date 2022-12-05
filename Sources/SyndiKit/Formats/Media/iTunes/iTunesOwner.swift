3777765645a0ca25a2cdd53df5a901a492315800
leogdion <leogdion@brightdigit.com>
2022-12-05 09:02:14 -0500

    making itunes owner email optional (#41)

--- Sources/SyndiKit/Formats/Media/iTunes/iTunesOwner.swift
+++ Sources/SyndiKit/Formats/Media/iTunes/iTunesOwner.swift
@@ -1,7 +1,7 @@
 // swiftlint:disable:next type_name
 public struct iTunesOwner: Codable {
   public let name: String
-  public let email: String
+  public let email: String?
 
   enum CodingKeys: String, CodingKey {
     case name = "itunes:name"
