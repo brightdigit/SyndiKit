2230fb06b9c00a1834f4a822761f910641efc60a
Leo Dion <leogdion@brightdigit.com>
2023-08-01 20:40:46 -0400

    using local packages

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/CoreOffice/XMLCoder", from: "0.16.0")
+    .package(url: "https://github.com/CoreOffice/XMLCoder", from: "0.17.1")
   ],
   targets: [
     .target(
