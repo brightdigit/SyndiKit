7d0259c7ecb0d5fdb2fa8c73704e60949169f850
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-09-18 21:05:46 +0300

    add: created, categories attributes to OPML (with custom decoding)

--- Sources/SyndiKit/Formats/OPML/OPMLOutline.swift
+++ Sources/SyndiKit/Formats/OPML/OPMLOutline.swift
@@ -10,6 +10,8 @@ public extension OPML {
     public let type: String?
     public let version: String?
     public let xmlUrl: String?
+    public let created: String?
+    public let categories: [String]?
 
     public let outlines: [Outline]?
 
@@ -22,8 +24,27 @@ public extension OPML {
       case type
       case version
       case xmlUrl
+      case created
+      case categories = "category"
 
       case outlines = ""
+    }
+
+    public init(from decoder: Decoder) throws {
+      let container: KeyedDecodingContainer<OPML.Outline.CodingKeys> = try decoder.container(keyedBy: OPML.Outline.CodingKeys.self)
+      self.text = try container.decode(String.self, forKey: OPML.Outline.CodingKeys.text)
+      self.description = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.description)
+      self.htmlUrl = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.htmlUrl)
+      self.language = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.language)
+      self.title = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.title)
+      self.type = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.type)
+      self.version = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.version)
+      self.xmlUrl = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.xmlUrl)
+      self.created = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.created)
+      self.outlines = try container.decodeIfPresent([OPML.Outline].self, forKey: OPML.Outline.CodingKeys.outlines)
+
+      let category = try container.decodeIfPresent(String.self, forKey: OPML.Outline.CodingKeys.categories)
+      self.categories = category?.components(separatedBy: ",")
     }
   }
 }
