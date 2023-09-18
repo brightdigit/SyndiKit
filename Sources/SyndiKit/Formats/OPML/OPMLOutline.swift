854ee2599d52c93cc9cb45704c4482e78d5cfef6
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-09-18 20:44:52 +0300

    fix: all Outline properties should be optional except text

--- Sources/SyndiKit/Formats/OPML/OPMLOutline.swift
+++ Sources/SyndiKit/Formats/OPML/OPMLOutline.swift
@@ -3,13 +3,13 @@ import Foundation
 public extension OPML {
   struct Outline: Codable, Equatable {
     public let text: String
-    public let description: String
-    public let htmlUrl: String
-    public let language: String
-    public let title: String
-    public let type: String
-    public let version: String
-    public let xmlUrl: String
+    public let description: String?
+    public let htmlUrl: String?
+    public let language: String?
+    public let title: String?
+    public let type: String?
+    public let version: String?
+    public let xmlUrl: String?
 
     public let outlines: [Outline]?
 
