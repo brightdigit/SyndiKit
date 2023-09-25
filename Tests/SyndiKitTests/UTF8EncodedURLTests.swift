ed5151e4bb3e7db297eafe664cb69d967e171580
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-09-25 09:52:16 +0300

    test: UTF8EncodedURL, XMLStringInt

---
+++ Tests/SyndiKitTests/UTF8EncodedURLTests.swift
@@ -0,0 +1,22 @@
+@testable import SyndiKit
+import XMLCoder
+import XCTest
+
+internal final class UTF8EncodedURLTests: XCTestCase {
+  internal func testDecode() throws {
+    let expectedURL = URL(strict: "http://www.example.com/index.php")!
+    let urlStr = """
+    "\(expectedURL)"
+    """
+
+    guard let data = urlStr.data(using: .utf8) else {
+      XCTFail("Expected data out of \(urlStr)")
+      return
+    }
+
+    let sut = try JSONDecoder().decode(UTF8EncodedURL.self, from: data)
+
+    XCTAssertEqual(sut.value, expectedURL)
+    XCTAssertNil(sut.string)
+  }
+}
