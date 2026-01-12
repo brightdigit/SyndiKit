3638be139767626d29861512a11e3038f863cc99
Leo Dion <leogdion@brightdigit.com>
2026-01-12 09:10:56 -0500

    reverting a specific version of wasmtime

--- Tests/SyndiKitTestSupport/Extensions/FileManager.swift
+++ Tests/SyndiKitTestSupport/Extensions/FileManager.swift
@@ -6,13 +6,42 @@
 
 extension FileManager {
   internal func dataFromDirectory(at sourceURL: URL) throws -> [(String, Result<Data, Error>)] {
-    let urls = try contentsOfDirectory(
-      at: sourceURL,
-      includingPropertiesForKeys: nil,
-      options: []
-    )
+    #if os(WASI)
+      // WASM: Use explicit file list instead of contentsOfDirectory
+      // Directory enumeration is not supported in WASI (POSIX error 58)
+      // but individual file reading works fine
+      let directoryName = sourceURL.lastPathComponent
+      let filenames: [String]
 
-    return urls.mapPairResult { try Data(contentsOf: $0) }
-      .map { ($0.0.deletingPathExtension().lastPathComponent, $0.1) }
+      switch directoryName {
+      case "XML":
+        filenames = TestFileManifests.xmlFiles
+      case "JSON":
+        filenames = TestFileManifests.jsonFiles
+      case "OPML":
+        filenames = TestFileManifests.opmlFiles
+      case "WordPress":
+        filenames = TestFileManifests.wordPressFiles
+      default:
+        throw CocoaError(.fileNoSuchFile)
+      }
+
+      return filenames.map { filename in
+        let url = sourceURL.appendingPathComponent(filename)
+        let result = Result { try Data(contentsOf: url) }
+        let key = url.deletingPathExtension().lastPathComponent
+        return (key, result)
+      }
+    #else
+      // macOS/Linux: Use contentsOfDirectory as normal
+      let urls = try contentsOfDirectory(
+        at: sourceURL,
+        includingPropertiesForKeys: nil,
+        options: []
+      )
+
+      return urls.mapPairResult { try Data(contentsOf: $0) }
+        .map { ($0.0.deletingPathExtension().lastPathComponent, $0.1) }
+    #endif
   }
 }
